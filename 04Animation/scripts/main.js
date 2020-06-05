82191249d49df6e51fa20b2c5eceb5b4492e64d6
Danique <daniquediks@gmail.com>
2020-06-05 23:03:21 +0200

    update right box

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,9 +6,10 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
-	console.log(myOtherBox.object3D.rotation.y);
-	console.log(myOtherBox.object3D.rotation.z);
+	
 	
 }
 
